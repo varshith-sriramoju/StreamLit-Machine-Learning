b9963ff8320ef86c073503334c1e44385af07657
Varshith <97534851+varshith-sriramoju@users.noreply.github.com>
2025-08-22 22:12:19 +0530

    Comment out unused sklearn imports
    Comment out unused imports for clarity.

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve
-from sklearn.metrics import precision_score,recall_score
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.preprocessing import LabelEncoder
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve
+#from sklearn.metrics import precision_score,recall_score
 def main():
     st.title('Binary Classification Web App')
 
